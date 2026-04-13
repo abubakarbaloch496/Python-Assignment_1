25430b68939999c68a893b67ff49ef77a78295f0
abubakarbaloch496 <abubakarbaloch626@gmail.com>
2026-04-13 08:55:03 -0100

    Assignment_1 Task_4
    # Calculates and prints the perimeter and area of a circle

---
+++ Circle.py
@@ -0,0 +1,7 @@
+# Calculates the perimeter and area of a circle
+radius= 5.5
+p= 3.14
+perimeter=2*radius*p
+area=radius*radius*p
+print("Perimeter of the circle",perimeter)
+print("Area of the circle",area)
